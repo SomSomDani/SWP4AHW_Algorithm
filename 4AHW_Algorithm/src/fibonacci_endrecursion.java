0a3d3ca8a366ac26e23993602db7444b823aa6c5
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2020-11-04 14:18:55 +0100

    Baumkonstrukt bei Fibonacci

--- 4AHW_Algorithm/src/fibonacci_endrecursion.java
+++ 4AHW_Algorithm/src/fibonacci_endrecursion.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class fibonacci_endrecursion {
 	static Scanner scanner = new Scanner(System.in);
-	static double n;
+	static int n;
 	public static void main(String[] args)
 	{
 		System.out.println("Geben Sie eine Zahl ein: ");
-		n = scanner.nextDouble(); 
+		n = scanner.nextInt(); 
 		System.out.println(fibonacci(n));
 	}
-	public static int endrecursionFibonacci(double n, int fbn1, int fbn2)
+	public static int endrecursionFibonacci(int n, int fbn1, int fbn2)
 	{
+		System.out.println(n + " " + fbn1 + " " + fbn2);
 		if(n<=1)
 		{
 			return fbn1+fbn2;
@@ -20,7 +21,7 @@ public class fibonacci_endrecursion {
 			return endrecursionFibonacci(n-1, fbn2+fbn1, fbn1);
 		}
 	}
-	public static  int fibonacci(double n)
+	public static  int fibonacci(int n)
 	{
 		return endrecursionFibonacci(n-1,1,0);
 	}
