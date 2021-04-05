7ba9307b366dee5bb8b1c479dfa4b1056e5d58f6
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2021-04-05 20:56:04 +0200

    changing selectionsort

--- 4AHW_Algorithm/SelectionSort/src/SelectionSort.java
+++ 4AHW_Algorithm/SelectionSort/src/SelectionSort.java
@@ -1,96 +1,195 @@
-import java.util.ArrayList;
 
 public class SelectionSort {
     static int countSelectionSort;
     static int countSelectionSortStable;
     static int countBubbleSort;
-    static int countAVGSelectionSort;
-    static int countAVGSelectionSortStable;
-    static int countAVGBubbleSort;
+    static int swapSelectionSort;
+    static int swapSelectionSortStable;
+    static int swapBubbleSort;
+    static int countInsertionSort;
+    static int swapInsertionSort;
+    static double calc;
+    static long dauer1;
+    static long dauer2;
+    static long dauer3;
+    static long dauer4;
+    static long startdauer1;
+    static long startdauer2;
+    static long startdauer3;
+    static long startdauer4;
+    static long enddauer1;
+    static long enddauer2;
+    static long enddauer3;
+    static long enddauer4;
+
     public static void main(String[] args) {
-        for(int i = 0; i <= 100; i++)
-        {
-            int[] arr = new int[10000];
-            int n = arr.length;
-            int range = 1000;
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = (int) (Math.random() * range);
-            }
-            bubbleSort(arr,n);
-            selectionSort(arr,n);
-            selectionSortStable(arr,n);
+        for (int i = 0; i <= 0; i++) {
+            int[] arr = new int[100];
+            int range = 10;
+
+            randomArray(arr, range);
+            bubbleSort(arr);
+            average(arr);
+            //printArray(arr);
+
+            randomArray(arr, range);
+            selectionSort(arr);
+            average(arr);
+            //printArray(arr);
+
+            randomArray(arr, range);
+            selectionSortStable(arr);
+            average(arr);
+            //printArray(arr);
+
+            randomArray(arr, range);
+            insertionSort(arr);
+            average(arr);
+            //printArray(arr);
 
         }
-        System.out.println(countSelectionSort);
-        System.out.println(countSelectionSortStable);
-        System.out.println(countBubbleSort);
-        System.out.println(countAVGSelectionSort);
-        System.out.println(countAVGSelectionSortStable);
-        System.out.println(countAVGBubbleSort);
+        System.out.println("UnstableSelektionSort: ");
+        System.out.println("Vergleichoperationen: " + countSelectionSort);
+        System.out.println("Tauschoperationen: " + swapSelectionSort);
+        System.out.println("Dauer: " + dauer1);
+        System.out.println("");
+
+        System.out.println("SelektionSortStable: ");
+        System.out.println("Vergleichoperationen: " + countSelectionSortStable);
+        System.out.println("Tauschoperationen: " + swapSelectionSortStable);
+        System.out.println("Dauer: " + dauer2);
+        System.out.println("");
+
+        System.out.println("BubbleSort: ");
+        System.out.println("Vergleichoperationen: " + countBubbleSort);
+        System.out.println("Tauschoperationen: " + swapBubbleSort);
+        System.out.println("Dauer: " + dauer3);
+        System.out.println("");
+
+        System.out.println("InsertionSort: ");
+        System.out.println("Vergleichoperationen: " + countInsertionSort);
+        System.out.println("Tauschoperationen: " + swapInsertionSort);
+        System.out.println("Dauer: " + dauer4);
+        System.out.println("");
     }
-    public static int selectionSort(int arr[], int n)
-    {
+
+    public static int selectionSort(int[] arr) {
+        startdauer1 = System.nanoTime();
         countSelectionSort = 0;
-        for(int i= 0;i<n-1;i++)
-        {
+        swapSelectionSort = 0;
+        for (int i = 0; i < arr.length; i++) {
             int min_idx = i;
-            for(int j = i+1;j<n;j++)
-            {
-                if(arr[j] < arr[min_idx])
-                {
+            for (int j = i + 1; j < arr.length; j++) {
+                // Anzahl der Vergleiche
+                countSelectionSort++;
+                if (arr[j] < arr[min_idx]) {
                     min_idx = j;
                 }
-                int temp = arr[min_idx];
-                arr[min_idx] = arr[i];
-                arr[i] = temp;
-                countSelectionSort++;
             }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+            swapSelectionSort++;
         }
-        countAVGSelectionSort = (countSelectionSort/100);
-        return countSelectionSort;
+        enddauer1 = System.nanoTime();
+        dauer1 = enddauer1 - startdauer1;
+        return 0;
     }
-    public static int selectionSortStable(int arr[],int n)
-    {
+
+    public static int selectionSortStable(int[] arr) {
+        startdauer2 = System.nanoTime();
         countSelectionSortStable = 0;
-        for (int i = 0; i<n-1;i++)
-        {
+        swapSelectionSortStable = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
             int min = i;
-            for (int j = i+1; j <n;j++)
-            {
-                if(arr[min]>arr[j])
-                {
+            for (int j = i + 1; j < arr.length; j++) {
+                // Anzahl der Vergleiche
+                countSelectionSortStable++;
+                if (arr[min] > arr[j]) {
+
                     min = j;
                 }
-                int key = arr[min];
-                while(min>i)
-                {
-                    arr[min] = arr[min-1];
-                    min--;
-                }
-                arr[i] = key;
-                countSelectionSortStable++;
+            }
+            for (int k = min; k > i; k--) {
+                int temp = arr[k];
+                arr[k] = arr[k-1];
+                arr[k-1] = temp;
+                swapSelectionSortStable++;
             }
         }
-        countAVGSelectionSortStable = (countSelectionSortStable / 100);
-        return countSelectionSortStable;
+        enddauer2 = System.nanoTime();
+        dauer2 = enddauer2 - startdauer2;
+        return 0;
     }
-    public static int bubbleSort(int arr[], int n)
-    {
-        countBubbleSort= 0;
-        for (int i = 0; i< n-1; i++)
-        {
-            for(int j = 0; j<n-i-1;j++)
-            {
-                if(arr[j] > arr[j+1])
-                {
+
+    public static int bubbleSort(int[] arr) {
+        startdauer3 = System.nanoTime();
+        countBubbleSort = 0;
+        swapBubbleSort = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                // Anzahl der Vergleiche
+                countBubbleSort++;
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    countBubbleSort++;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    // Anzahl der Tauschoperationen
+                    swapBubbleSort++;
                 }
             }
         }
-        countAVGBubbleSort = (countBubbleSort/100);
-        return countBubbleSort;
+        enddauer3 = System.nanoTime();
+        dauer3 = enddauer3 - startdauer3;
+        return 0;
+    }
+
+    public static int insertionSort(int[] arr) {
+        startdauer4 = System.nanoTime();
+        countInsertionSort = 0;
+        swapInsertionSort = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            countInsertionSort++;
+            while (j >= 0 && arr[j] > key) {
+                countInsertionSort++;
+                swapInsertionSort++;
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        swapInsertionSort++;
+        enddauer4 = System.nanoTime();
+        dauer4 = enddauer4 - startdauer4;
+        return 0;
+    }
+
+    public static double average(int[] arr) {
+        calc = 0;
+        for (int i = 0; i < arr.length; i++) {
+            calc = calc + arr[i];
+        }
+        return (calc / arr.length);
+    }
+
+    public static double median(int[] arr) {
+        calc = arr[arr.length / 2];
+        return calc;
+    }
+
+    private static void printArray(int[] arr) {
+        for (int a : arr) {
+            System.out.print(a);
+            System.out.print(",");
+        }
+        System.out.println("");
+    }
+
+    private static void randomArray(int[] arr, int range) {
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = (int) (Math.random() * range);
+        }
     }
 }
